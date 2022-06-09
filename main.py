7ca156e3eee322c3e74745615429ec590a761c83
bmt-dev <66754509+bmt-dev@users.noreply.github.com>
2022-06-09 14:00:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from tkinter import *
+from tkinter import filedialog as fd
 import os
 
 fields = ('Chemin', 'Heure début', 'Date', 'Fps', 'Dossier de sortie')
 
+chemin_field = None
+
 def makeform(root, fields):
+   global chemin_field
    entries = {}
    for field in fields:
       row = Frame(root)
       lab = Label(row, width=13, text=field+": ", anchor='w')
       ent = Entry(row)
+      if field == fields[0]:
+          chemin_field = ent
       row.pack(side = TOP, fill = X, padx = 5 , pady = 5)
       lab.pack(side = LEFT)
       ent.pack(side = RIGHT, expand = YES, fill = X)
@@ -33,7 +39,7 @@ def add_date_time(entries):
    
    video_out = os.path.join(out_dir, os.path.basename(video_in))
    
-   command = "ffmpeg -i \"{input}\" -vf \"drawtext=timecode='{tc}':r={fps}:x=(w-tw)/2:y=h-(lh):fontsize=30: text={take_date}: box=1:boxcolor=black@0.8:fontcolor=white\" -c:a copy \"{output}\"".format(input=video_in, output=video_out, tc=timecode, fps=framerate, take_date=video_date + '-')
+   command = "ffmpeg -i \"{input}\" -vf \"drawtext=timecode='{tc}':r={fps}:x=(w-tw)/2:y=h-(lh):fontsize=50: text={take_date}: box=1:boxcolor=black@0.8:fontcolor=white\" -c:a copy \"{output}\"".format(input=video_in, output=video_out, tc=timecode, fps=framerate, take_date=video_date + '-')
             
    print(command) # debugging only
    
@@ -41,15 +47,25 @@ def add_date_time(entries):
    
    os.startfile(out_dir)
    
+def select_file():
+   filetypes = (
+	   ('Video files', '*.mp4;*.flv;*.avi;*.mkv'),
+	   ('All files', '*.*')
+   )
+
+   filename = fd.askopenfilename(title='Selectionner une vidéo', initialdir='/', filetypes=filetypes)
    
-   
-   
+   if chemin_field:
+	   chemin_field.insert(0, filename)
+
 
 if __name__ == '__main__':
    root = Tk()
    root.title('Timestamper')
    ents = makeform(root, fields)
    root.bind('<Return>', (lambda event, e = ents: fetch(e)))
+   b_chemin = Button(root, text = 'Choisir un fichier', command=select_file)
+   b_chemin.pack(side = LEFT, padx = 5, pady = 5)
    b1 = Button(root, text = 'Démarrer',
       command=(lambda e = ents: add_date_time(e)))
    b1.pack(side = LEFT, padx = 5, pady = 5)
